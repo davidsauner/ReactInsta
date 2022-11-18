3f0fa975e16878fa18300465ba16fd74f7729112
David <davidsauner@gmail.com>
2022-11-18 14:21:36 -0300

    componentizado

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Corpo from "./Corpo/Corpo";
+import NavBar from "./NavBar/NavBar";
+
+export default function App() {
+  return (
+    <>
+      <NavBar />
+      <Corpo />
+    </>
+  );
+}
